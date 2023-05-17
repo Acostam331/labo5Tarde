288fdfb337d4677403b985b27fb03f8f78f4f844
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2023-05-17 13:10:44 -0600

    Add: Prueba 2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int restar(int, int);
+int mult(int, int);
 
 int main()
 {
     int m = 5, n = 10;
 
-    cout << restar(m, n);
+    cout << mult(m, n);
 
     return 0;
 }
 
-int restar(int m, int n)
+int mult(int m, int n)
 {
-    return m - n;
+    return m * n;
 }
