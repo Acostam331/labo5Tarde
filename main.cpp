7293949e81fc857cd0301a127e70a3158209e320
José Miguel Acosta Vásquez <62571916+Acostam331@users.noreply.github.com>
2023-05-17 12:56:12 -0600

    Add; Test

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
-
 using namespace std;
 
-int main(){
-    
+int sumar(int, int);
+
+int main()
+{
+    int m = 5, n = 10;
+
+    cout << sumar(m, n);
+
     return 0;
+}
+
+int sumar(int m, int n)
+{
+    return m + n;
 }
